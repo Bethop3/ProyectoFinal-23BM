48c5fc48218b5dc36a0cbe44a4ce6a618a432961
Christopher <Christodrc@gmail.com>
2023-07-10 13:29:09 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinal-23BM/Vistas/Sistema.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ProyectoFinal_23BM.Entities;
+using ProyectoFinal_23BM.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_23BM.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para Sistema.xaml
+    /// </summary>
+    public partial class Sistema : Window
+    {
+        public Sistema()
+        {
+            InitializeComponent();
+        }
+        UsuarioServices services = new UsuarioServices();
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nombre = txtNombre.Text;
+            usuario.UserName = txtUserName.Text;
+            usuario.Password = txtPassword.Text;
+            services.AddUser(usuario);
+        }
+    }
+}
---
+++ ProyectoFinal-23BM/services/UsuarioServices.cs
@@ -0,0 +1,58 @@
+﻿using ProyectoFinal_23BM.Context;
+using ProyectoFinal_23BM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_23BM.services
+{
+    public class UsuarioServices
+    {
+        public void AddUser(Usuario request)
+        {
+            try
+            {
+                if(request != null)
+                {
+                    using (var _context = new ApplicationDbContext())
+                    {
+                        Usuario res = new Usuario();
+                        res.Nombre = request.Nombre;
+                        res.UserName = request.UserName;
+                        res.Password = request.Password;
+                        _context.Usuarios.Add(res);
+                        _context.SaveChanges();
+
+                    }
+                }
+
+            }catch (Exception ex)
+            {
+                throw new Exception("Sucedio un error "+ex.Message);
+            }
+        }
+        public List<Usuario > GetUsuarios()
+        {
+            try
+            {
+                using(var _context = new ApplicationDbContext())
+                {
+                    List<Usuario> res = _context.Usuarios.ToList();
+                    if(res.Count > 0)
+                    {
+                        return res;
+                    }
+                    return res;
+
+                }
+
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
